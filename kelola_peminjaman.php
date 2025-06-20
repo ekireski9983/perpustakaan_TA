9991498a8f37052b7b4c99bfb81946083e44acf1
ekireski9983 <rezkyjayadis@gmail.com>
2025-06-20 18:03:37 +0700

    update kelola peminjaman

--- kelola_peminjaman.php
+++ kelola_peminjaman.php
@@ -112,19 +112,37 @@ if (isset($_GET['id'])) {
     </div>
   </div>
 
-  <div class="row">
+ <div class="row">
       <nav class="col-md-3 d-none d-md-block sidebar min-vh-100 position-relative">
         <h5 class="pt-4">Pustakawan<br /><small>admin</small></h5>
         <a href="kelola_anggota.php">Kelola Anggota</a>
         <a href="kelola_katalog.php">Kelola Katalog Buku</a>
         <a href="kelola_peminjaman.php">Kelola Peminjaman Buku</a>
         <a href="kelola_pengembalian.php">Kelola Pengembalian Buku</a>
-        <a href="kelola_denda.php">Kelola denda</a>
+        <a href="kelola_denda.php">Kelola Denda</a>
         <a href="logout.php">Logout</a>
         <div class="image-box text-center mt-5">
           <img src="assets/Bootstrap_logo.png" alt="icon" />
         </div>
       </nav>
+      
+      <div class="offcanvas offcanvas-start sidebar text-white" tabindex="-1" id="sidebarMenu">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title">Pustakawan</h5>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <div class="offcanvas-body">
+          <a href="kelola_anggota.php">kelola anggota</a> 
+          <a href="kelola_katalog.php">kelola katalog buku</a>
+          <a href="kelola_peminjaman.php">kelola Peminjaman buku</a>
+          <a href="kelola_pengembalian.php">kelola Pengembalian buku</a>
+          <a href="kelola_denda.php">kelola denda</a>
+          <a href="logout.php">Logout</a>
+          <div class="image-box text-center mt-5">
+            <img src="assets/Bootstrap_logo.png" alt="icon" />
+          </div>
+        </div>
+      </div>
 
     <main class="col-md-9 col-12 main-content">
       <h4>Kelola Peminjaman Buku</h4>
